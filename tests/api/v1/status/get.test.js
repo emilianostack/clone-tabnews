848a1d37da345fe36c06fd0941863a23493e6fef
Emiliano <emilianousb@gmail.com>
2025-01-08 01:03:08 +0000

    Adicionado arquivos  e

--- tests/api/v1/status/get.test.js
+++ tests/api/v1/status/get.test.js
@@ -1,4 +1,7 @@
+const database = require("../../../../infra/database");
+
 test("Get to /api/v1/status should return 200", async () => {
+  const result = await database.query("select 1 + 1");
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
 });
