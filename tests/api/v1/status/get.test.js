9d9180bd54bf0b3b8a716d840a08713c47d6f281
Emiliano <emilianousb@gmail.com>
2025-01-13 00:06:30 +0000

    Finalizado o endpoint

--- tests/api/v1/status/get.test.js
+++ tests/api/v1/status/get.test.js
@@ -1,7 +1,16 @@
-const database = require("infra/database");
+const database = require("../../../../infra/database.js");
 
 test("Get to /api/v1/status should return 200", async () => {
-  const result = await database.query("select 1 + 1");
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+  expect(responseBody.updated_at).toBeDefined();
+
+  const parseUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parseUpdatedAt);
+
+  expect(responseBody.dependencies.database.version).toEqual("16.0");
+  expect(responseBody.dependencies.database.max_connections).toEqual(100);
+  expect(responseBody.dependencies.database.opened_connections).toEqual(1);
 });
