7a76b490afae9ee65b05173fb829cac3921eab93
Emiliano <emilianousb@gmail.com>
2025-02-12 17:56:21 +0000

    Feito o > clone-tabnews@1.0.0 test > npm run services:up && npm run wait-for-postgres && concurrently -n next,jest --hide next -k -s command-jest 'next dev' 'jest --runInBand'
    > clone-tabnews@1.0.0 services:up
    > docker compose -f infra/compose.yaml up -d
    
    > clone-tabnews@1.0.0 wait-for-postgres
    > node infra/scripts/wait-for-postgres.js
    
     🔴 Aguardando Postgres aceitar conexão
     🟢 Postgres está pronto e aguardando conexões!
    
    [jest] PASS tests/api/v1/migrations/post.test.js
    [jest] PASS tests/api/v1/migrations/get.test.js
    [jest] PASS tests/api/v1/status/get.test.js
    [jest]
    [jest] Test Suites: 3 passed, 3 total
    [jest] Tests:       3 passed, 3 total
    [jest] Snapshots:   0 total
    [jest] Time:        1.941 s, estimated 2 s
    [jest] Ran all test suites.
    [jest] jest --runInBand exited with code 0
    --> Sending SIGTERM to other processes.. mais robusto com o  and

--- tests/api/v1/status/get.test.js
+++ tests/api/v1/status/get.test.js
@@ -1,4 +1,9 @@
 const database = require("../../../../infra/database.js");
+import orchestrator from "../orchestrator.js";
+
+beforeAll(async () => {
+  await orchestrator.waitForAllServices();
+});
 
 test("Get to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
