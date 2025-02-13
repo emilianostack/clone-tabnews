ae77e24ff4b5de05e58c01d6f71926c1daafa0fb
Emiliano <emilianousb@gmail.com>
2025-02-13 17:28:12 +0000

    Melhorado performace no > clone-tabnews@1.0.0 test > npm run services:up && concurrently -n next,jest --hide next -k -s command-jest 'next dev' 'jest --runInBand --verbose'
    > clone-tabnews@1.0.0 services:up
    > docker compose -f infra/compose.yaml up -d
    
    [jest] PASS tests/api/v1/migrations/post.test.js
    [jest]   ✓ Post to /api/v1/migrations should return 200 (143 ms)
    [jest]
    [jest] PASS tests/api/v1/status/get.test.js
    [jest]   ✓ Get to /api/v1/status should return 200 (33 ms)
    [jest]
    [jest] PASS tests/api/v1/migrations/get.test.js
    [jest]   ✓ Get to /api/v1/migrations should return 200 (28 ms)
    [jest]
    [jest] Test Suites: 3 passed, 3 total
    [jest] Tests:       3 passed, 3 total
    [jest] Snapshots:   0 total
    [jest] Time:        1.736 s, estimated 2 s
    [jest] Ran all test suites.
    [jest] jest --runInBand --verbose exited with code 0
    --> Sending SIGTERM to other processes..

--- tests/api/v1/orchestrator.js
+++ tests/api/v1/orchestrator.js
@@ -6,6 +6,7 @@ async function waitForAllServices() {
   async function waitForWebServer() {
     return retry(fetchStatusPage, {
       retries: 100,
+      maxTimeout: 10000,
     });
 
     async function fetchStatusPage() {
