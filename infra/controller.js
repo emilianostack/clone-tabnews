2a7fc09399a9b981a397fb7aa1b19cf8d18fa2ec
Emiliano <emilianousb@gmail.com>
2026-03-27 18:43:44 +0000

    refactor: definir SameSite=Lax no cookie de sessão

--- infra/controller.js
+++ infra/controller.js
@@ -40,6 +40,7 @@ function setSessionCookie(sessionToken, response) {
     maxAge: session.EXPIRATION_IN_MILLISECONDS / 1000,
     secure: process.env.NODE_ENV === "production",
     httpOnly: true,
+    sameSite: "Lax",
   });
 
   response.setHeader("Set-Cookie", setCookie);
@@ -51,6 +52,7 @@ function clearSessionCookie(response) {
     maxAge: -1,
     secure: process.env.NODE_ENV === "production",
     httpOnly: true,
+    sameSite: "Lax",
   });
 
   response.setHeader("Set-Cookie", setCookie);
--- tests/integration/api/v1/sessions/delete.test.js
+++ tests/integration/api/v1/sessions/delete.test.js
@@ -106,6 +106,7 @@ describe(`DELETE /api/v1/sessions`, () => {
         maxAge: -1,
         path: "/",
         httpOnly: true,
+        sameSite: "Lax",
       });
 
       const doubleCheckResponse = await fetch(
